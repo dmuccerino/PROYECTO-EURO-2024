a8211c865fd31a5d29f702d60affaefd3bfbe646
dmuccerino <dmuccerino@gmail.com>
2024-07-01 01:03:00 -0400

    a

--- controllers/match_controller.py
+++ controllers/match_controller.py
@@ -1,19 +1,8 @@
 
-from models.match import Match
-
 class MatchController:
-    def __init__(self):
-        self.matches = []
+    def __init__(self, team_controller, stadium_controller):
+        self.team_controller = team_controller
+        self.stadium_controller = stadium_controller
+       
 
-    def agregar_partido(self, home_team, away_team, date, stadium):
-        match = Match(home_team, away_team, date, stadium)
-        self.matches.append(match)
 
-    def get_matches_by_team(self, team_name):
-        return [match for match in self.matches if match.home_team.name == team_name or match.away_team.name == team_name]
-
-    def get_matches_by_stadium(self, stadium_name):
-        return [match for match in self.matches if match.stadium.name == stadium_name]
-
-    def get_matches_by_date(self, date):
-        return [match for match in self.matches if match.date == date]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.api_utils import cargar_datos_iniciales
 
 def main():
     team_controller, stadium_controller, match_controller = cargar_datos_iniciales()
-    # Aquí iría la lógica para manejar la interacción del usuario
+    
 
 if __name__ == "__main__":
     main()
